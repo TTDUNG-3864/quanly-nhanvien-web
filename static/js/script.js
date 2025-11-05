06e00cf27b749b095503d49fc210c77e4d24377a
TTDUNG-3864 <konhotk206@gmail.com>
2025-11-05 12:24:19 +0700

    Add full project with CSS/JS

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // Xác nhận trước khi xóa nhân viên
 document.querySelectorAll('.delete-btn').forEach(btn => {
     btn.addEventListener('click', function(e) {
-        if(!confirm("Bạn có chắc muốn xóa nhân viên này không?")) {
+        if (!confirm("Bạn có chắc muốn xóa nhân viên này không?")) {
             e.preventDefault(); // Ngăn chặn link
         }
     });
